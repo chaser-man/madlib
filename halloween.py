335c50e1c43a83b79490cbae814517cd3f3defeb
chaser-man <95667028+chaser-man@users.noreply.github.com>
2024-10-16 15:10:30 +0000

    commit

--- halloween.py
+++ halloween.py
@@ -1,1 +1,12 @@
-print("hello, world!")
+complete_story_paragraph_one = " <male_name> loves his <color_one> bike. he was going to <adverb_one> ride it to <city_one>, but he saw an extremely <adjective_one> skeleten. Naturally, he grabbed his trusty <construction_tool> and began building a <adjective_two> bike jump to go over the spooky skeleten. it took him <pi> days to build it. <verb_one> to his bike, as he climbed onto his <color_one> bike, he felt something <adjective_three> tap his back. it was the <adjective_one> skeleton! <male_name> was face to face with the skeleton. 💀"# for reference
+
+male_name = input("enter a male name: ")
+color_one = input("enter a color: ")
+adverb_one = input("enter an adverb: ")
+city_one = input("enter a city: ")
+adjective_one = input ("enter an adjective: ")
+contruction_tool = input("enter a construction tool: ")
+adjective_two = input("enter an adjective: ")
+pi = input("list as many digits of pi as you can (3.14 if you dont know any): ")
+verb_one = input("enter a verb: ")
+
